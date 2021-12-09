d7f262026b0487c99f49baa672d61699b259bc4f
unknown <jul.pichon@gmail.com>
2021-12-09 16:24:32 +0100

    +img  lovers-controller et lovers.php

--- views/lovers.php
+++ views/lovers.php
@@ -14,23 +14,41 @@ foreach($femmes as $femme) : ?>
 <!-- si la clé existe et a pour valeur "vrai", on affiche -->
 <?php if (array_key_exists('is_enabled', $femme) && $femme['is_enabled'] == true): ?>
         <div class="col pt-3">
-            <div class="card" style="width: 18rem;">
+            <div class="card border border-5 border-light" style="width: 18rem;">
                 <img src="../assets/img/femmes/<?= $femme['picture'] ?>" class="card-img-top" alt="profile picture">
                 <div class="card-body">
-                    <p class="card-text"><?= $femme['lastname']; ?></p>
-                    <p class="card-text"><?= $femme['firstname']; ?></p>
-                    <p class="card-text"><?= $femme['age']; ?></p>
-                    <p class="card-text"><?= $femme['zipcode']; ?></p>
+                    <p class="card-text"><span class="text-uppercase fw-bold"><?= $femme['lastname']; ?></span> <?= $femme['firstname']; ?>, <span class="small text-end"><?= $femme['age']; ?> ans</span></p>
                     <p class="card-text"><?= $femme['description']; ?></p>
-
-                    <a href="#" class="btn btn-primary">Like</a>
+                    <p class="card-text small"><?= $femme['zipcode']; ?></p>
+                    <a href="#" class="btn btn-danger rounded-circle" ><i class="bi bi-heart"></i></a>
                 </div>
             </div>
         </div>
    <?php endif;
 endforeach
-
 ?>
+
+<?php 
+
+foreach($hommes as $homme) : ?>
+<!-- si la clé existe et a pour valeur "vrai", on affiche -->
+<?php if (array_key_exists('is_enabled', $homme) && $homme['is_enabled'] == true): ?>
+        <div class="col pt-3">
+            <div class="card border border-5 border-light" style="width: 18rem;">
+                <img src="../assets/img/hommes/<?= $homme['picture'] ?>" class="card-img-top" alt="profile picture">
+                <div class="card-body">
+                    <p class="card-text"><span class="text-uppercase fw-bold"><?= $homme['lastname']; ?></span> <?= $homme['firstname']; ?>, <span class="small text-end"><?= $homme['age']; ?> ans</span></p>
+                    <p class="card-text"><?= $homme['description']; ?></p>
+                    <p class="card-text small"><?= $homme['zipcode']; ?></p>
+                    <a href="#" class="btn btn-danger rounded-circle" ><i class="bi bi-heart"></i></a>
+                </div>
+            </div>
+        </div>
+   <?php endif;
+endforeach
+?>
+
+
     </div>
     
 </div>

d7ef852173529c6b2dd55873bc9ef1a5b12f16c8
unknown <jul.pichon@gmail.com>
2021-12-11 19:52:51 +0100

    push lovers-controller et lovers.php

--- controllers/lovers-controller.php
+++ controllers/lovers-controller.php
@@ -1,4 +1,5 @@
 <?php
+
 // femmes
 $femmes = [
     [
@@ -10,7 +11,6 @@ $femmes = [
     'zipcode' => '94200',
     'picture' => '1.jpg',
     'description' => 'Nullam vitae ex porta, molestie ligula dictum, elementum massa. Nulla vitae quam est. Mauris bibendum odio et ex hendrerit varius. Integer sit amet facilisis justo.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Chaloux',
@@ -21,7 +21,6 @@ $femmes = [
     'zipcode' => '83300',
     'picture' => '96.jpg',
     'description' => 'Etiam accumsan ornare nibh, at sollicitudin velit feugiat non. In gravida fermentum dui, vel tristique magna aliquam eget. Duis vitae ex felis. Nunc porttitor quam ac elit blandit, ut iaculis tellus venenatis.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Petrie',
@@ -32,7 +31,6 @@ $femmes = [
     'zipcode' => '13015',
     'picture' => '7.jpg',
     'description' => 'Nullam fringilla, nibh non pulvinar accumsan, felis ante maximus ligula, viverra feugiat lorem lacus venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Morin',
@@ -43,7 +41,6 @@ $femmes = [
     'zipcode' => '80000',
     'picture' => '20.jpg',
     'description' => 'Fusce purus massa, accumsan id orci ut, accumsan ullamcorper turpis. Aenean metus risus, consequat vitae volutpat at, pharetra at mauris.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Jupiler',
@@ -54,7 +51,6 @@ $femmes = [
     'zipcode' => '76000',
     'picture' => '21.jpg',
     'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut voluptas sequi nesciunt vitae nemo illo reiciendis porro ex maxime eius saepe laborum, perspiciatis ipsa possimus odit quasi totam, quam eligendi.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Goudal',
@@ -65,7 +61,6 @@ $femmes = [
     'zipcode' => '76000',
     'picture' => '27.jpg',
     'description' => 'Felis ante maximus ligula, viverra feugiat lorem lacus venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Orval',
@@ -76,7 +71,6 @@ $femmes = [
     'zipcode' => '76000',
     'picture' => '16.jpg',
     'description' => 'Integer sit amet facilisis justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'Labrosse',
@@ -87,7 +81,6 @@ $femmes = [
     'zipcode' => '76000',
     'picture' => '100.jpg',
     'description' => 'Non pulvinar accumsan, felis ante maximus ligula, viverra feugiat lorem lacus venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec ac lacus eu ante scelerisque ornare.',
-    'is_enabled' => true,
     ],
 ];
 
@@ -101,7 +94,6 @@ $hommes = [
     'zipcode' => '76000',
     'picture' => '14.jpg',
     'description' => 'Integer sit amet facilisis justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'DOE',
@@ -112,7 +104,6 @@ $hommes = [
     'zipcode' => '76000',
     'picture' => '29.jpg',
     'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut voluptas sequi nesciunt vitae nemo illo reiciendis porro ex maxime eius saepe laborum, perspiciatis ipsa possimus odit quasi totam, quam eligendi.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'DOE',
@@ -123,7 +114,6 @@ $hommes = [
     'zipcode' => '76000',
     'picture' => '34.jpg',
     'description' => 'Aut voluptas sequi nesciunt vitae nemo illo reiciendis porro ex maxime eius saepe laborum, perspiciatis ipsa possimus odit quasi totam, quam eligendi.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'DOE',
@@ -134,7 +124,6 @@ $hommes = [
     'zipcode' => '76000',
     'picture' => '45.jpg',
     'description' => 'Nullam fringilla, nibh non pulvinar accumsan, felis ante maximus ligula, viverra feugiat lorem lacus venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames.',
-    'is_enabled' => true,
     ],
     [
     'lastname' => 'DOE',
@@ -145,43 +134,6 @@ $hommes = [
     'zipcode' => '76000',
     'picture' => '46.jpg',
     'description' => 'Viverra feugiat lorem lacus venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames.',
-    'is_enabled' => true,
     ],
 ];
 
-
-foreach($femmes as $femme) : 
-if (array_key_exists('is_enabled', $femme) && $femme['is_enabled'] == true): ?>
-        <div class="col pt-3">
-            <div class="card border border-5 border-light" style="width: 18rem;">
-                <img src="../assets/img/femmes/<?= $femme['picture'] ?>" class="card-img-top" alt="profile picture">
-                <div class="card-body">
-                    <p class="card-text"><span class="text-uppercase fw-bold"><?= $femme['lastname']; ?></span> <?= $femme['firstname']; ?>, <span class="small text-end"><?= $femme['age']; ?> ans</span></p>
-                    <p class="card-text"><?= $femme['description']; ?></p>
-                    <p class="card-text small">Département : <?= $femme['zipcode']; ?></p>
-                    <a href="#" class="btn btn-white border border-danger rounded-circle" ><i class="bi bi-heart text-danger"></i></a>
-                </div>
-            </div>
-        </div>
-   <?php endif;
-endforeach
-?>
-
-<?php 
-
-foreach($hommes as $homme) : 
- if (array_key_exists('is_enabled', $homme) && $homme['is_enabled'] == true): ?>
-        <div class="col pt-3">
-            <div class="card border border-5 border-light" style="width: 18rem;">
-                <img src="../assets/img/hommes/<?= $homme['picture'] ?>" class="card-img-top" alt="profile picture">
-                <div class="card-body">
-                    <p class="card-text"><span class="text-uppercase fw-bold"><?= $homme['lastname']; ?></span> <?= $homme['firstname']; ?>, <span class="small text-end"><?= $homme['age']; ?> ans</span></p>
-                    <p class="card-text"><?= $homme['description']; ?></p>
-                    <p class="card-text small">Département : <?= $homme['zipcode']; ?></p>
-                    <a href="#" class="btn btn-white border border-danger rounded-circle" ><i class="bi bi-heart text-danger"></i></a>
-                </div>
-            </div>
-        </div>
-   <?php endif;
-endforeach
-?>

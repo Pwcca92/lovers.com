819ff761e39f35608c0d15d97c32091e55921ffd
Pwcca92 <ggaudre@icloud.com>
2021-12-12 17:44:26 +0100

    modification index et lovers-controllers

--- index.php
+++ index.php
@@ -1,88 +1,98 @@
 <!-- include header + navbar -->
-<?php include_once('views/header.php');
- ?>
-<div class="container">
-    <!-- Titre -->
-    <div class="row">
-        <h2 class="titre">Inscription</h2>
-    </div>
-    <!-- Fomulaire -->
-    <form action="controllers/index-controller.php" method="post" enctype="multipart/form-data">
-        <div class="row p-4">
+<?php include('views/header.php'); ?>
 
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <input type="text" name="lastname" class="form-control" id="inputname" placeholder="" value="">
-                    <label for="inputname">Nom</label>
-                   
-                </div>
-            </div>
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <input type="text" name="firstname" class="form-control" id="inputfname" value="">
-                    <label for="inputfname">Prénom</label>
-                   
-                </div>
+<div class="container mt-2 d-flex justify-content-center">
+    <!-- Titre -->
+    <div class="card w-75">
+        <div class="card-header">
+            <div class="row">
+                <h2 class="title">Inscription</h2>
             </div>
         </div>
-        <div class="row p-4">
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <select class="form-select" type="text" name="gender1" aria-label="Default select example" value="">
-                        <option selected>Vous êtes</option>
-                        <option value="homme">Homme</option>
-                        <option value="femme">Femme</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <select class="form-select" type="text" name="gender2" aria-label="Default select example" value="">
-                        <option selected>Vous recherchez</option>
-                        <option value="homme">Homme</option>
-                        <option value="femme">Femme</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <div class="row p-4">
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <input type="date" name="age" class="form-control" id="inputdate" value="">
-                    <label for="inputdate"></label>  
-                </div>
-            </div>
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <input type="text" name="zipcode" class="form-control" id="inputpostal" placeholder="" value="">
-                    <label for="inputpostal">Code postal</label>    
-                </div>
-            </div>
-        </div>
-        <div class="row p-4">
-            <div class="col-lg-6 col-sm-12 py-2">
-                 
-                <div class="form-floating">                  
-                    <input type="email" name="mail" class="form-control" id="email" aria-describedby="emailHelp">
-                    <label for="email" class="form-label">mail@mail.com</label>
-                    <div id="emailHelp" class="form-text">Nous ne partagerons votre e-mail.</div>
-                </div>
-            </div>
-            <div class="col-lg-6 col-sm-12 py-2">
-                <div class="form-floating">
-                    <div>
-                        <label for="formFile" class="form-label">Choisir une photo de profil</label>
-                        <input class="form-control" type="file" name="picture" id="formFile">
+
+        <div class="card-body">
+            <!-- Fomulaire -->
+            <form action="controllers/user-controller.php" method="post" enctype="multipart/form-data">
+                <div class="row p-2">
+                    <div class="col-lg-6 col-sm-12 sm-py-2 py-auto">
+                        <div class="form-floating">
+                            <input type="text" name="lastname" class="form-control" id="inputname" placeholder="" value="">
+                            <label for="floatingInputGrid">Nom</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12 sm-py-2 py-auto">
+                        <div class="form-floating">
+                            <input type="text" name="firstname" class="form-control" id="inputfname" value="">
+                            <label for="floatingSelectGrid">Prénom</label>
+                        </div>
                     </div>
                 </div>
+
+                <div class="row p-2">
+                    <div class="col-lg-6 col-sm-12 py-auto">
+                        <div class="form-floating">
+                            <select class="form-select" type="text" name="gender1" aria-label="Default select example" value="">
+                                <option selected value="1">Homme</option>
+                                <option value="2">Femme</option>
+                            </select>
+                            <label for="floatingSelectGrid">Votre genre :</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12 py-auto">
+                        <div class="form-floating">
+                            <select class="form-select" type="text" name="gender2" aria-label="Default select example" value="">
+                                <option value="1">Homme</option>
+                                <option selected value="2">Femme</option>
+                            </select>
+                            <label for="floatingSelectGrid">Vous recherchez :</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row p-2">
+                    <div class="col-lg-6 col-sm-12 py-auto">
+                        <div class="form-floating">
+                            <input type="date" name="age" class="form-control" id="inputdate" value="">
+                            <label for="floatingSelectGrid">Date de naissance</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12 py-auto">
+                        <div class="form-floating">
+                            <input type="text" name="zipcode" class="form-control" id="inputpostal" placeholder="" value="">
+                            <label for="floatingInputGrid">Code postal</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row p-2">
+                    <div class="col-lg-6 col-sm-12 py-auto">
+                        <div class="form-floating">
+                            <input type="email" name="mail" class="form-control" id="Email1" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text text-center">Nous ne partagerons votre e-mail.</div>
+                            <label for="floatingInputGrid">Votre mail</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12 py-auto">
+                        <div class="form-floating">
+                            <div>
+                                <input accept="" class="form-control" type="file" name="picture" id="formFile">
+                                <label for="formFile" class="form-label">Choisir une photo de profil</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="card-footer">
+            <div class="row px-3 pt-1 pb-1">
+                <div class="col d-flex flex-row-reverse">
+                    <button class="btn btn-primary btn-lg" type="submit">Valider</button>
+                </div>
             </div>
         </div>
-        <div class="row px-4 pt-2 pb-3">
-            <div class="col">
-                <button class="btn btn-primary btn-lg" type="submit">Valider</button>
-            </div>
-    </form>
-</div>
+        </form>
+    </div>
 </div>
 
 <?php include_once('views/footer.php'); ?>
